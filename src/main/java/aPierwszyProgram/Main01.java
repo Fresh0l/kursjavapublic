4d4da7cbacb94f0aee00a651203627cf3b09c4b2
kurs <p.swiezy@zstio3.pl>
2021-10-28 17:00:18 +0200

    fkoajfkl

---
+++ src/main/java/aPierwszyProgram/Main01.java
@@ -0,0 +1,10 @@
+package aPierwszyProgram;
+
+
+//      psvm
+public class Main01 {
+    public static void main(String[] args) {
+        //      sout
+        System.out.println("Mój Pierwszy Program W Java");
+    }
+}
